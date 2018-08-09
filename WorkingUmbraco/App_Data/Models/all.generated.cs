89765477eb78a8780c28b9628ff679f580b23d8b
bikokwa <peterbikokwa@gmail.com>
2018-08-09 22:44:22 +0300

    Contact Page form

--- WorkingUmbraco/App_Data/Models/all.generated.cs
+++ WorkingUmbraco/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "6f69c865e484c24c")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
