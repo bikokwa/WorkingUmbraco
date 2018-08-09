f1ea5cd9e12d96f35be6deb7675f0b48d8de26b4
bikokwa <peterbikokwa@gmail.com>
2018-08-09 22:50:00 +0300

    Contact model

---
+++ WorkingUmbraco/Models/ContactModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkingUmbraco.Models
+{
+    public class ContactModel
+    {
+        //id
+        public int Id { get; set; }
+
+        //email
+        public string Email { get; set; }
+
+        //Name
+        public string Name { get; set; }
+
+        //Message
+        public string Message { get; set; }
+    }
+}
