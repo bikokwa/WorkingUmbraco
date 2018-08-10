c1d5e4618329898eee690b4d0350e56a7c5bf0aa
bikokwa <peterbikokwa@gmail.com>
2018-08-10 08:32:21 +0300

    Controller, ContactForm

---
+++ WorkingUmbraco/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using WorkingUmbraco.Models;
+
+namespace WorkingUmbraco.Controllers
+{
+    public class ContactSurfaceController: SurfaceController 
+    {
+        public ActionResult ShowForm()
+        {
+            return PartialView("ContactForm", new ContactModel());
+        }
+    }
+}
